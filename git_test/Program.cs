c6c4c20f899c777299cefacaacfdb5f2d194daac
OriPoria <58737667+OriPoria@users.noreply.github.com>
2022-02-05 00:33:24 +0200

    master 00:33 empty file!

--- git_test/Program.cs
+++ git_test/Program.cs
@@ -6,25 +6,6 @@ namespace git_test
     {
         static void Main(string[] args)
         {
-            int loops = 20;
-            for (int i = 0; i < loops; i++)
-            {
-                if (i > 5 && i < 10)
-                {
-                    Console.WriteLine(i);
-                }
-            }
-            int k = 1;
-            if (k == 2)
-            {
-                Console.WriteLine(j);   
-            }
-            while (k < 23)
-            {
-                Console.WriteLine(k);
-            }
-            Console.WriteLine("End of loop!");
-            Console.WriteLine("Written by Ori");
         }
     }
 }
