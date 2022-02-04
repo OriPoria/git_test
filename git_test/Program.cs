7c0aed065a5554c36d1e7c966d4b4b0cb9220053
OriPoria <58737667+OriPoria@users.noreply.github.com>
2022-02-04 23:28:52 +0200

    branch1 23:28 add last line of complete loop

--- git_test/Program.cs
+++ git_test/Program.cs
@@ -6,17 +6,12 @@ namespace git_test
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             int loops = 10;
             for (int i = 0; i < loops; i++)
             {
                 Console.WriteLine(i);
             }
-=======
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-
->>>>>>> e11e32c... first
+            Console.WriteLine("End of loop!");
         }
     }
 }
