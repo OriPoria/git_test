3778982aac5f759e26320e843f397305a38bbd3d
OriPoria <58737667+OriPoria@users.noreply.github.com>
2022-02-04 23:25:16 +0200

    branch1 23:24 resolve conflicts from rebase

--- git_test/Program.cs
+++ git_test/Program.cs
@@ -6,11 +6,17 @@ namespace git_test
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             int loops = 10;
             for (int i = 0; i < loops; i++)
             {
                 Console.WriteLine(i);
             }
+=======
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+>>>>>>> e11e32c... first
         }
     }
 }
