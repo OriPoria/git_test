84d61613eba95c35af896e31fa59d0fd74e0e3ef
OriPoria <58737667+OriPoria@users.noreply.github.com>
2022-02-06 22:31:46 +0200

    22:31 dev add test print

--- git_test/Program.cs
+++ git_test/Program.cs
@@ -9,7 +9,7 @@ namespace git_test
             Console.WriteLine("Hello world");
             Console.WriteLine("Start development");
             Console.WriteLine("Start feature");
-
+            Console.WriteLine("Test");
 
         }
     }
