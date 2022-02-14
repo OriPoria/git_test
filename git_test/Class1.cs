b57f57420cb485cc75660bfc2ef125dc9ab2e4a9
OriPoria <58737667+OriPoria@users.noreply.github.com>
2022-02-14 21:20:07 +0200

    dev 21:19 fun func

---
+++ git_test/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace git_test
+{
+    class Class1
+    {
+        int fun()
+        {
+            return 0;
+        }
+    }
+}
